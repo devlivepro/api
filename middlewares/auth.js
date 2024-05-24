1a9f3fc681b90c824c3be77fee1a54f0c3fabae2
david <tyson.b@hotmail.fr>
2024-05-24 11:30:39 +0200

    Updates

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function(req, res, next) {
+module.exports = (req, res, next) => {
   const token = req.header('Authorization').replace('Bearer ', '');
-  if (!token) return res.status(401).send('Access Denied');
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. No token provided.' });
+  }
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
     next();
-  } catch (error) {
-    res.status(400).send('Invalid Token');
+  } catch (ex) {
+    res.status(400).json({ error: 'Invalid token.' });
   }
 };
+
